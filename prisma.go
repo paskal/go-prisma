0708fe64b1b5b40ccab01d5a531241feb5be4a8e
Dmitry Verkhoturov <paskal.07@gmail.com>
2020-02-16 11:37:07 +0100

    make doAPIRequestWithToken standalone function

--- prisma.go
+++ prisma.go
@@ -63,14 +63,53 @@ func (p *API) DoAPIRequest(method, url string, body io.Reader) ([]byte, error) {
 			return nil, errors.Wrap(err, "error getting auth token")
 		}
 	}
-	return p.doAPIRequestWithToken(method, url, p.token, body)
+	return doAPIRequestWithToken(method, p.apiURL+url, p.token, body)
+}
+
+// authenticate gets or renews the API authentication token
+// https://api.docs.prismacloud.io/reference#login
+func (p *API) authenticate() error {
+	p.tokenRenewTime = time.Now()
+	var res = &authResponse{}
+	switch p.token {
+	case "":
+		// no token set yet, first login
+		loginData := map[string]string{"username": p.username, "password": p.password}
+		jsonValue, err := json.Marshal(loginData)
+		if err != nil {
+			return errors.Wrap(err, "error marshaling login data")
+		}
+		data, err := doAPIRequestWithToken("POST", p.apiURL+"/login", "", bytes.NewBuffer(jsonValue))
+		if err != nil {
+			return errors.Wrapf(err, "error logging in with user %q", p.username)
+		}
+		if err := json.Unmarshal(data, res); err != nil {
+			return errors.Wrap(err, "error obtaining token from login response")
+		}
+		p.token = res.Token
+	default:
+		// token is set and we will try to renew it
+		data, err := doAPIRequestWithToken("GET", p.apiURL+"/auth_token/extend", p.token, nil)
+		if err != nil {
+			log.Printf("[INFO] Error extending token, will re-login, %v", err)
+			p.token = ""
+			return p.authenticate()
+		}
+		if err := json.Unmarshal(data, res); err != nil {
+			log.Printf("[INFO] Error obtaining token from extend token response, will re-login, %v", err)
+			p.token = ""
+			return p.authenticate()
+		}
+		p.token = res.Token
+	}
+	return nil
 }
 
 // doAPIRequestWithToken does request to API with specified method and
 // provided token and returns response body on success.
 // Thread safe.
-func (p *API) doAPIRequestWithToken(method, url, token string, body io.Reader) ([]byte, error) {
-	req, err := http.NewRequest(method, p.apiURL+url, body)
+func doAPIRequestWithToken(method, fullURL, token string, body io.Reader) ([]byte, error) {
+	req, err := http.NewRequest(method, fullURL, body)
 	if err != nil {
 		return nil, errors.Wrap(err, "error creating request")
 	}
@@ -98,43 +137,4 @@ func (p *API) doAPIRequestWithToken(method, url, token string, body io.Reader) (
 	default:
 		return nil, errors.Errorf("%v, response body: %q", response.Status, data)
 	}
-}
-
-// authenticate gets or renews the API authentication token
-// https://api.docs.prismacloud.io/reference#login
-func (p *API) authenticate() error {
-	p.tokenRenewTime = time.Now()
-	var res = &authResponse{}
-	switch p.token {
-	case "":
-		// no token set yet, first login
-		loginData := map[string]string{"username": p.username, "password": p.password}
-		jsonValue, err := json.Marshal(loginData)
-		if err != nil {
-			return errors.Wrap(err, "error marshaling login data")
-		}
-		data, err := p.doAPIRequestWithToken("POST", "/login", "", bytes.NewBuffer(jsonValue))
-		if err != nil {
-			return errors.Wrapf(err, "error logging in with user %q", p.username)
-		}
-		if err := json.Unmarshal(data, res); err != nil {
-			return errors.Wrap(err, "error obtaining token from login response")
-		}
-		p.token = res.Token
-	default:
-		// token is set and we will try to renew it
-		data, err := p.doAPIRequestWithToken("GET", "/auth_token/extend", p.token, nil)
-		if err != nil {
-			log.Printf("[INFO] Error extending token, will re-login, %v", err)
-			p.token = ""
-			return p.authenticate()
-		}
-		if err := json.Unmarshal(data, res); err != nil {
-			log.Printf("[INFO] Error obtaining token from extend token response, will re-login, %v", err)
-			p.token = ""
-			return p.authenticate()
-		}
-		p.token = res.Token
-	}
-	return nil
 }
